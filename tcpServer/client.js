51638732d48860c6203e11773db4745d4d2da973
AlinDos <alina.listunova@gmail.com>
2016-05-21 23:27:59 +0300

    Task [1]

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -1,18 +1,32 @@
 var api = {};
 global.api = api;
+// Connect an asynchronous network wrapper
 api.net = require('net');
 
+// Create socket
 var socket = new api.net.Socket();
-var user;
 
-socket.connect({
-  port: 2000,
+// Connect our socket to 2000 port on local host
+socket.connect( {
+  port: 2000, 
   host: '127.0.0.1',
 }, function() {
-  socket.write('Hello from client');
-  socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+  // Listen to data from server
+  socket.on('data', (data) => {
+    
+    // Turn JSON text into JS-object
+    task = JSON.parse(data);
+    
+    // Print our task
+    console.log('Data received (by client): ' + task);
+    
+    // Do some calculations
+    results = task.map((num) => {return num*2});
+    
+    // Send our results formatted to JSON to server
+    socket.write(JSON.stringify(results));
+    
+    // Close socket
+    socket.end();
   });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,13 +1,33 @@
 var api = {};
 global.api = api;
+// Connect an asynchronous network wrapper
 api.net = require('net');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+// Numbers to process
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+// Our result
+var results = [];
 
-var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
+// Create server listening 2000 port
+var server = api.net.createServer((socket) => {
+  
+  // Print address client connected on
   console.log('Connected: ' + socket.localAddress);
-  socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
+  
+  // Send our task formatted to JSON to client 
+  socket.write(JSON.stringify(task));
+  
+  // Listen to data from client
+  socket.on('data', (data) => {
+    // Turn JSON text into JS-object
+    results = JSON.parse(data);
+    // Print our results
+    console.log('Data received (by server): ' + results);
   });
+  
+  // Listen to FIN packet from client
+  socket.on('end', () => {
+    console.log('Connection ended');
+  });
+  
 }).listen(2000);
